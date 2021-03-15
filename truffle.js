a508226be885ec95e1ae3f1e23b9711d47b57799
Nagy Salem <me@muhnagy.com>
2021-03-15 19:51:56 +0200

    updated compiler version in truffle

--- truffle.js
+++ truffle.js
@@ -2,6 +2,11 @@ var HDWalletProvider = require("truffle-hdwallet-provider");
 var mnemonic = ""; //put your mnemonic for the account that will deploy the contract to rinkeby here
 
 module.exports = {
+  compilers: {
+    solc: {
+      version: "^0.4.23", // A version or constraint - Ex. "^0.5.0"
+    	},
+     },
   networks: {
     development: {
       host: "127.0.0.1",
